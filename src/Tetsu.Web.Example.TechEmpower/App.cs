eec4f01b57abe569a433ad4b019c98317c1e0f76
Gokberk Yaltirakli <webdosusb@gmail.com>
2019-06-29 06:30:22 +0100

    Parse request headers

--- src/Tetsu.Web.Example.TechEmpower/App.cs
+++ src/Tetsu.Web.Example.TechEmpower/App.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace Tetsu.Web.Example.TechEmpower {
@@ -14,6 +15,13 @@ namespace Tetsu.Web.Example.TechEmpower {
                 ctx.Response.TextContent = JsonConvert.SerializeObject(new {
                     message = "Hello, World!"
                 });
+            });
+
+            server.AddMiddleware(ctx => {
+                Console.WriteLine($"URL = {ctx.Request.Uri}");
+
+                var ua = ctx.Request.Headers.Single(x => x.Item1 == "User-Agent").Item2;
+                Console.WriteLine($"User Agent = {ua}");
             });
 
             server.Listen("127.0.0.1", 1234).Wait();
